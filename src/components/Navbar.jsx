d9704f233752f6be8980c9c6bf1f0e0d4331784e
kaylachen24 <kaylachen24@gmail.com>
2025-06-25 23:20:25 -0700

    made changes to files, added navbar and home page

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-name">kayla chen</div>
+      <ul className="navbar-menu">
+        <li>about</li>
+        <li>experience</li>
+        <li>projects</li>
+        <li>contact</li>
+      </ul>
+    </nav>
+);
+};
+
+export default Navbar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <section id="about">
+      <h2>hi! my name is kayla</h2>
+      <p>I'm a second-year Computer Science student at the University of Florida.</p>
+    </section>
+  );
+}
+
+export default Home;
