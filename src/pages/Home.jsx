14ac8c610f3e72c0113d5f174e1e921f3aebc849
kaylachen24 <kaylachen24@gmail.com>
2025-06-26 22:39:18 -0700

    updated home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,22 @@ import './Home.css';
 
 function Home() {
   return (
+    <>
     <section id="about">
       <h2>hi! my name is kayla</h2>
       <p>I'm a second-year Computer Science student at the University of Florida.</p>
     </section>
+
+    <section id="experience">
+        <li>Treasurer @ CS Kickstart</li>
+        <li>Sports Coordinator @ Society of Asian Scientists and Engineers</li>
+    </section>
+
+    <section id="projects">
+        <h2>below are some of the projects i've worked on so far!</h2>
+        <p>i'm interested in fullstack development</p>
+    </section>
+    </>
   );
 }
 
